8289bc3b7d422a0adfc526d447d01a6ba171cf74
Kely Rufino <87963324+kely-rufino@users.noreply.github.com>
2022-02-28 19:21:29 +0000

    gif search init commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 function App() {
+  const [gifList, setGifList] = useState([]);
+  const [searchCriteria, setSearchCriteria] = useState("");
+
+  useEffect(() => {
+    const searchGiphy = async () => {
+      const response = await fetch(
+        `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${searchCriteria}&limit=10&offset=0&rating=g&lang=en`
+      );
+      const data = await response.json();
+      setGifList(data.data || []);
+    };
+    searchGiphy();
+  }, [searchCriteria]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input
+          onChange={(e) => {
+            setSearchCriteria(e.target.value);
+          }}
+          type="text"
+          id="name"
+          name="name"
+        />
+        <div className="searchResults">
+          {gifList.map((gif) => {
+            return <img src={gif.images.preview_gif.url} alt="" />;
+          })}
+        </div>
       </header>
     </div>
   );
